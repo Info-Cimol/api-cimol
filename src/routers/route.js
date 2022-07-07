0b3aa3ec83c22a4c5e0b31dac99b6c688bcdf7fb
TailiniSoaresWinck <tailini-swinck@educar.rs.gov.br>
2022-07-07 19:17:53 -0300

    Realização das rotas Curriculo

--- src/routers/route.js
+++ src/routers/route.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const userController=require("../controller/userController");
 const cursoController=require("../controller/cursoController");
+const curriculoController=require("../controller/curriculoController");
 const router = express.Router();
 
 router.get('/', (req, res, next)=>{
@@ -31,5 +32,22 @@ router.put('/curso/:idCurso', async(req, res, next)=>{
   resp=await cursoController.put (req.headers, req.body, req.params.idCurso );
   res.status(200).send(resp);
 })
+router.get('/alunos', async(req, res, next)=>{
+  //curso=await cursoController.get(req.headers);
+  res.status(200).send({"status":"ok"});
+})
 
+router.get('/alunos/curriculo/:idAluno', async(req, res, next)=>{
+  curriculo=await curriculoController.get(req.headers,  req.params.idAluno);
+  res.status(200).send(curriculo);
+})
+router.put('/alunos/curriculo/:idAluno/:idcurriculo', async(req, res, next)=>{
+  resp=await curriculoController.put (req.headers, req.body, req.params.idAluno,req.params.idcurriculo );
+  res.status(200).send(resp);
+})
+
+router.post('/alunos/curriculo', async(req, res, next)=>{
+  resp=await curriculoController.post(req.headers, req.body);
+  res.status(200).send(resp);
+})
 module.exports=router;
